682b83cfa39ef3713872f88250b26185f7495a44
Lee-sung-il <wj132612@naver.com>
2025-04-17 10:56:59 +0900

    오즈 스쿨 12일차 과제

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,10 +1,20 @@
 export default function Content({ content }) {
-  return (
-    <div className="content-container">
-      <img src={content.img} alt={content.title} />
-      <span>모집중</span>
-      <div>{content.title}</div>
-      <p>{content.subtitle}</p>
-    </div>
-  );
+    return (
+        <div className="flex flex-col items-start gap-2 border p-4 rounded-lg shadow-md">
+            <div className="relative w-full">
+                <img
+                    className="w-full rounded-[10px]"
+                    src={content.img}
+                    alt={content.title}
+                />
+                <span className="absolute top-2 left-2 text-[12px] text-[#d7fa00] border border-[#d7fa00] px-[4px] py-[2px] rounded-[3px] bg-black bg-opacity-60">
+          모집중
+        </span>
+            </div>
+            <div className="w-full">
+                <div className="text-lg font-semibold">{content.title}</div>
+                <p className="text-sm text-gray-500">{content.subtitle}</p>
+            </div>
+        </div>
+    );
 }
